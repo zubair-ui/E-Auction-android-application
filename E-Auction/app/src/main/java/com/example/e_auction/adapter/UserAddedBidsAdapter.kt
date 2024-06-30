e8cd3724e0e838f2eeb0607a92c11ae48e7247c1
Zubair Ali Bhutto <bhuttozubair6@gmail.com>
2024-06-30 16:45:32 +0500

    Update UserAddedBidsAdapter.kt

--- E-Auction/app/src/main/java/com/example/e_auction/adapter/UserAddedBidsAdapter.kt
+++ E-Auction/app/src/main/java/com/example/e_auction/adapter/UserAddedBidsAdapter.kt
@@ -2,7 +2,6 @@ package com.example.e_auction.adapter
 
 import android.annotation.SuppressLint
 import android.app.Activity
-import android.app.AlertDialog
 import android.app.ProgressDialog
 import android.content.Context
 import android.content.Intent
@@ -13,10 +12,12 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import android.widget.Toast
 import androidx.recyclerview.widget.RecyclerView
-import com.example.e_auction.AddOrUpdateBidActivity
+import com.example.e_auction.HomeActivity
+import com.example.e_auction.PlacedBidsActivity
 import com.example.e_auction.contract.Bid
 import com.example.e_auction.contract.Request
 import com.example.e_auction.contract.Response
+import com.example.e_auction.databinding.ItCurrentOrUserPlacedBidsBinding
 import com.example.e_auction.databinding.ItUserAddedBidsBinding
 import com.example.e_auction.network.IRequestContract
 import com.example.e_auction.network.NetworkClient
@@ -25,34 +26,28 @@ import com.example.e_auction.util.DataProvider
 import retrofit2.Call
 import retrofit2.Callback
 
-class UserAddedBidsAdapter(var activity: Activity, var context: Context, var dataSource: MutableList<Bid>) :
-    RecyclerView.Adapter<UserAddedBidsAdapter.UserAddedBidsViewHolder>(), Callback<Response> {
+class CurrentOrUserPlacedBidsAdapter(var activity: Activity, var context: Context, var dataSource: MutableList<Bid>):
+    RecyclerView.Adapter<CurrentOrUserPlacedBidsAdapter.CurrentOrUserPlacedBidsViewHolder>(),
+    Callback<Response> {
 
     private var progressDialog: ProgressDialog = ProgressDialog(context)
     private val retrofitClient = NetworkClient.getNetworkClient()
     private val requestContract = retrofitClient.create(IRequestContract::class.java)
-    private lateinit var deletedBid: Bid
-    private var deletedPosition: Int = -1
+    private lateinit var updatedBid: Bid
+    private var updatedPosition: Int = -1
 
-    private fun base64ToBitmap(base64String: String): Bitmap? {
-        // Decode Base64 string to byte array
-        val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
-
-        // Create Bitmap from byte array
-        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
-    }
-
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): UserAddedBidsViewHolder {
-        val view = ItUserAddedBidsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return UserAddedBidsViewHolder(view)
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CurrentOrUserPlacedBidsViewHolder {
+        val binding = ItCurrentOrUserPlacedBidsBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        return CurrentOrUserPlacedBidsViewHolder(binding)
     }
 
     override fun getItemCount(): Int {
         return dataSource.size
     }
 
-    override fun onBindViewHolder(holder: UserAddedBidsViewHolder, @SuppressLint("RecyclerView") position: Int) {
+    override fun onBindViewHolder(holder: CurrentOrUserPlacedBidsViewHolder,@SuppressLint("RecyclerView") position: Int) {
         val bid = dataSource[position]
+        holder.bind(bid)
         holder.title.text = bid.title
         holder.image.setImageBitmap(base64ToBitmap(bid.image))
         holder.description.text = bid.description
@@ -62,57 +57,82 @@ class UserAddedBidsAdapter(var activity: Activity, var context: Context, var dat
         holder.highestBid.text = bid.highestBid.toString()
         holder.highestBidder.text = bid.highestBidderEmail
 
-        holder.btnEdit.setOnClickListener {
-            Intent(context, AddOrUpdateBidActivity::class.java).apply {
-                DataProvider.bid = bid
-                putExtra(Constant.KEY_REASON, 2)  //2 means Edit
-                activity.startActivity(this)
+        holder.submitButton.setOnClickListener {
+            try{
+                val userBidInt = holder.userBid.text.toString().toInt()
+                val highestBidInt = holder.highestBid.text.toString().toInt()
+                val startingBidInt = holder.startingBid.text.toString().toInt()
+
+                if(startingBidInt<=userBidInt){
+                    if(userBidInt<highestBidInt){
+                        context.showToast("Please Enter valid value")
+                    }
+                    else{
+                        progressDialog.setMessage("Please wait...")
+                        progressDialog.setCancelable(false)
+                        updatedBid = bid
+                        updatedPosition = position
+                        val request = Request(
+                            action = Constant.UPDATE_HIGHEST_BIDDER,
+                            userEmail = DataProvider.userEmail,
+                            bidId = bid.bidId,
+                            highestBid = userBidInt.toString()
+                        )
+                        progressDialog.show()
+                        val callResponse = requestContract.makeApiCall(request)
+                        callResponse.enqueue(this)
+                        holder.bind(bid)
+                    }
+                }
+                else{
+                    context.showToast("Please Enter valid value")
+                }
+            }catch (_:NumberFormatException ){
+                context.showToast("Please enter a valid number")
             }
+
         }
 
-        holder.btnDelete.setOnClickListener {
-            AlertDialog.Builder(context)
-                .setTitle("Bid App Alert")
-                .setMessage("Are you sure you want to delete this bid?")
-                .setPositiveButton("Yes") { dialog, which ->
-                    progressDialog.setMessage("Please wait...")
-                    progressDialog.setCancelable(false)
-                    deletedBid = bid
-                    deletedPosition = position
-                    val request = Request(
-                        action = Constant.DELETE_BID_ITEM,
-                        userEmail = DataProvider.userEmail,
-                        bidId = bid.bidId
-                    )
-                    progressDialog.show()
-                    val callResponse = requestContract.makeApiCall(request)
-                    callResponse.enqueue(this)
-                }
-                .setNegativeButton("No") { dialog, which ->
-                    dialog?.dismiss()
-                }
-                .create()
-                .show()
+
+    }
+    private fun base64ToBitmap(base64String: String): Bitmap? {
+        // Decode Base64 string to byte array
+        val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
+
+        // Create Bitmap from byte array
+        return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+    }
+
+    class CurrentOrUserPlacedBidsViewHolder(private var binding: ItCurrentOrUserPlacedBidsBinding) : RecyclerView.ViewHolder(binding.root) {
+        var title = binding.tvTitle
+        var image = binding.imgItem
+        var description = binding.tvDescription
+        var startDate = binding.tvStartDate
+        var endDate = binding.tvEndDate
+        var startingBid = binding.tvStartingBid
+        var highestBid = binding.tvHighestBid
+        var highestBidder = binding.tvHighestBidder
+        var userBid = binding.etUserBid
+        var submitButton = binding.btBid
+    
+
+    fun bind(bid: Bid) {
+            binding.tvTitle.text = bid.title
+            binding.imgItem.setImageBitmap(base64ToBitmap(bid.image))
+            binding.tvDescription.text = bid.description
+            binding.tvStartDate.text = bid.startDate
+            binding.tvEndDate.text = bid.endDate
+            binding.tvStartingBid.text = bid.startingBid.toString()
+            binding.tvHighestBid.text = bid.highestBid.toString()
+            binding.tvHighestBidder.text = bid.highestBidderEmail
         }
-    }
+        private fun base64ToBitmap(base64String: String): Bitmap? {
+            // Decode Base64 string to byte array
+            val decodedBytes = Base64.decode(base64String, Base64.DEFAULT)
 
-    class UserAddedBidsViewHolder(view: ItUserAddedBidsBinding) : RecyclerView.ViewHolder(view.root) {
-        var title = view.tvTitle
-        var image = view.imgItem
-        var description = view.tvDescription
-        var startDate = view.tvStartDate
-        var endDate = view.tvEndDate
-        var startingBid = view.tvStartingBid
-        var highestBid = view.tvHighestBid
-        var highestBidder = view.tvHighestBidder
-        var btnEdit = view.imgEdit
-        var btnDelete = view.imgDelete
-    }
-
-    override fun onFailure(call: Call<Response>, t: Throwable) {
-        if (progressDialog.isShowing)
-            progressDialog.dismiss()
-        context.showToast("Server is not responding. Please contact your system administrator")
+            // Create Bitmap from byte array
+            return BitmapFactory.decodeByteArray(decodedBytes, 0, decodedBytes.size)
+        }
     }
 
     override fun onResponse(call: Call<Response>, response: retrofit2.Response<Response>) {
@@ -121,10 +141,10 @@ class UserAddedBidsAdapter(var activity: Activity, var context: Context, var dat
         if (response.body() != null) {
             val serverResponse = response.body()
             if (serverResponse!!.status) {
-                dataSource.remove(deletedBid)
-                notifyItemRemoved(deletedPosition)
-                notifyItemRangeChanged(deletedPosition, dataSource.size)
                 context.showToast(serverResponse.message)
+                val intent = Intent(context, PlacedBidsActivity::class.java)
+                context.startActivity(intent)
+                activity.finish()
             } else {
                 context.showToast(serverResponse.message)
             }
@@ -133,6 +153,11 @@ class UserAddedBidsAdapter(var activity: Activity, var context: Context, var dat
         }
     }
 
+    override fun onFailure(call: Call<Response>, t: Throwable) {
+        if (progressDialog.isShowing)
+            progressDialog.dismiss()
+        context.showToast("Server is not responding. Please contact your system administrator")
+    }
     private fun Context.showToast(message: String) {
         Toast.makeText(this, message, Toast.LENGTH_SHORT).show()
     }
